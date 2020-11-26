1a3d9df01f4950531d1a310c6f5259b7556a19c2
Luis Gonzalez Guzman <luisiiyoo@gmail.com>
2020-11-26 15:12:58 -0600

    Fix upload_all_data

--- upload_all_data.py
+++ upload_all_data.py
@@ -3,10 +3,12 @@ from os.path import isfile, join, splitext
 from typing import List
 import requests
 
-UPLOAD_MENUS = True
-UPLOAD_REGISTERS = False
-UPLOAD_MENUS_URL = "http://0.0.0.0:5050/menu/transform_file"
-UPLOAD_REGISTERS_URL = "http://0.0.0.0:5050/register/transform_file"
+UPLOAD_MENUS_FLAG = False
+UPLOAD_REGISTERS_FLAG = True
+TRANSFORM_MENUS_FILES_URL = "http://0.0.0.0:5050/dataset_creation/menu/transform_file"
+UPLOAD_MENUS_URL = "http://0.0.0.0:5050/dataset_creation/menu/insert"
+TRANSFORM_REGISTERS_FILES_URL = "http://0.0.0.0:5050/dataset_creation/register/transform_file"
+UPLOAD_REGISTERS_URL = "http://0.0.0.0:5050/dataset_creation/register/insert"
 MENUS_PATH = './data/MENU/'
 MENUS_EXTENSION = ['.csv', '.tsv']
 REGISTERS_PATH = './data/REGISTERS/'
@@ -21,7 +23,7 @@ def get_list_of_files(path: str, extensions: List[str] = []):
 
 
 # Main
-if UPLOAD_MENUS:
+if UPLOAD_MENUS_FLAG:
     print("Uploading Menus...")
     menu_files = get_list_of_files(MENUS_PATH, MENUS_EXTENSION)
     num_files = len(menu_files)
@@ -29,10 +31,11 @@ if UPLOAD_MENUS:
         with open(f"{MENUS_PATH}{file}", "rb") as a_file:
             print(f"{idx}/{num_files} -> File: {file}", end=' ')
             file_dict = {"file": a_file}
-            response = requests.post(UPLOAD_MENUS_URL, files=file_dict)
+            transform_response = requests.post(TRANSFORM_MENUS_FILES_URL, files=file_dict)
+            response = requests.post(UPLOAD_MENUS_URL, json=transform_response.json())
             print(response)
 
-if UPLOAD_REGISTERS:
+if UPLOAD_REGISTERS_FLAG:
     print("Uploading Registers...")
     register_files = get_list_of_files(REGISTERS_PATH, REGISTERS_EXTENSION)
     num_files = len(register_files)
@@ -40,6 +43,7 @@ if UPLOAD_REGISTERS:
         with open(f"{REGISTERS_PATH}{file}", "rb") as a_file:
             print(f"{idx+1}/{num_files} -> File: {file}", end=' ')
             file_dict = {"file": a_file}
-            response = requests.post(UPLOAD_REGISTERS_URL, files=file_dict)
+            transform_response = requests.post(TRANSFORM_REGISTERS_FILES_URL, files=file_dict)
+            response = requests.post(UPLOAD_REGISTERS_URL, json=transform_response.json())
             print(response)
 
